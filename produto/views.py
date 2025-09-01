a327dce84969ca39854369baa5562d2db3764ce4
Marcelo <marceloamaral1364@gmail.com>
2025-08-31 22:26:41 -0300

    feat: Adding product detail

--- produto/views.py
+++ produto/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.views import View
 from django.http import HttpResponse
 from . import models
@@ -12,9 +12,12 @@ class ListaProdutos(ListView):
     paginate_by = 10
 
 
-class DetalheProduto(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Detalhe')
+class DetalheProduto(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
+    
 
 
 class AdicionarCarrinho(View):
