b50cca323e8effeb931709fb3b71f0eaef384870
Marcelo <marceloamaral1364@gmail.com>
2026-01-01 21:30:23 -0300

    feat: Finishing pedido and listar pedido

--- pedido/urls.py
+++ pedido/urls.py
@@ -6,6 +6,6 @@ app_name = 'pedido'
 urlpatterns = [
     path('pagar/<int:pk>', views.Pagar.as_view(), name='pagar'),
     path('salvarpedido/', views.SalvarPedido.as_view(), name='salvar_pedido'),
-    path('detalhe/', views.Detalhe.as_view(), name='detalhe'),
+    path('detalhe/<int:pk>', views.Detalhe.as_view(), name='detalhe'),
     path('lista/', views.ListaPedidos.as_view(), name='lista'),
 ]
--- pedido/views.py
+++ pedido/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views.generic import View, DetailView
+from django.views.generic import View, DetailView, ListView
 from django.http import HttpResponse
 from django.contrib import messages
 from django.urls import reverse
@@ -10,7 +10,7 @@ from utils.utils import calculate_price_quantitative, cart_totals, total_qtd_car
 
 # Create your views here.
 
-class DispatchLoginRequired(View):
+class DispatchLoginRequiredMixin(View):
     def dispatch(self, request, *args, **kwargs):
 
         if not self.request.user.is_authenticated:
@@ -19,7 +19,7 @@ class DispatchLoginRequired(View):
         return super().dispatch(request, *args, **kwargs)
 
 
-class Pagar(DispatchLoginRequired, DetailView):
+class Pagar(DispatchLoginRequiredMixin, DetailView):
     template_name = 'pedido/pagar.html'
     model = Pedido
     pk_url_kwarg = 'pk'
@@ -120,10 +120,22 @@ class SalvarPedido(View):
         )
     
 
-class Detalhe(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Detalhe Pedido')
+class Detalhe(DispatchLoginRequiredMixin, DetailView):
+    model = Pedido
+    context_object_name = 'pedido'
+    template_name = 'pedido/detalhe.html'
 
-class ListaPedidos(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('ListarPedidos')
+
+class ListaPedidos(DispatchLoginRequiredMixin, ListView):
+    model = Pedido
+    context_object_name = 'pedidos'
+    template_name = 'pedido/lista.html'
+    paginate_by = 10
+    ordering = '-id',
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+
+        qs = qs.filter(usuario = self.request.user)
+
+        return qs    
